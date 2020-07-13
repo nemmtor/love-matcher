d7d02fb6847b81cafade723beab532791d5d075f
nemmtor <kacper0witas@gmail.com>
2020-07-13 18:50:40 +0200

    Result route now expects 2 search params: name1 and name2, if empty return warning

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import queryString from 'query-string';
 
-export default function Result() {
-  return <h1>Result</h1>;
+export default function Result({ location }) {
+  const { name1, name2 } = queryString.parse(location.search);
+  if (!name1 || !name2) return <p>Prease provide proper names!</p>;
+  return (
+    <>
+      <h1>Result</h1>
+      <p>{name1}</p>
+      <p>{name2}</p>
+    </>
+  );
 }
