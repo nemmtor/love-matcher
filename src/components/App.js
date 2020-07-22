6769a1b607e0bec499da167016be14320d5b1f0c
nemmtor <kacper0witas@gmail.com>
2020-07-22 13:31:08 +0200

    Adds hash router for netlify deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Error from './Error';
 import Layout from './Layout';
@@ -9,11 +9,13 @@ import Result from './Result';
 export default function App() {
   return (
     <Layout>
-      <Switch>
-        <Route exact path="/" component={LoveForm} />
-        <Route exact path="/result" component={Result} />
-        <Route component={Error} />
-      </Switch>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={LoveForm} />
+          <Route exact path="/result" component={Result} />
+          <Route component={Error} />
+        </Switch>
+      </HashRouter>
     </Layout>
   );
 }
