4458fa3fd75e32c6c6c8938a069ca89304bf863d
nemmtor <kacper0witas@gmail.com>
2020-07-22 13:24:02 +0200

    Adds fix for netlify deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Error from './Error';
 import Layout from './Layout';
@@ -9,13 +9,11 @@ import Result from './Result';
 export default function App() {
   return (
     <Layout>
-      <HashRouter>
-        <Switch>
-          <Route exact path="/" component={LoveForm} />
-          <Route exact path="/result" component={Result} />
-          <Route component={Error} />
-        </Switch>
-      </HashRouter>
+      <Switch>
+        <Route exact path="/" component={LoveForm} />
+        <Route exact path="/result" component={Result} />
+        <Route component={Error} />
+      </Switch>
     </Layout>
   );
 }
